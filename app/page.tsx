90f86df85fb09abd2bcc5aef4eb2899da736937c
Bahaa Ahmed <bahaaahmed19902012@gmail.com>
2026-09-11 23:01:26 +0300

    add hello world

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full  items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1>{process.env.VAR}</h1>
+        <br />
         <h1 className="capitalize text-5xl">next view demo</h1>
       </main>
     </div>
